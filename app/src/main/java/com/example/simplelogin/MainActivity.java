c85165af80814d523465d5928dd8ace300bbdd03
VLQAn <anoddeyes@gmail.com>
2024-09-12 20:24:42 +0700

    init project

---
+++ app/src/main/java/com/example/simplelogin/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.simplelogin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private Button loginBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginBtn = findViewById(R.id.login_btn);
+
+        addAcction();
+
+    }
+
+    private void addAcction() {
+        loginBtn.setOnClickListener(this::onClick);
+    }
+
+    public void onClick(View view) {
+        String username = usernameInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        Toast.makeText(MainActivity.this, "Username: " + username + " Password: " + password, Toast.LENGTH_SHORT).show();
+    }
+}
